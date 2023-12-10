8877742f384673b6b3eabdacbcce2da60f7c9665
Luis Artavia <luva.arso@outlook.com>
2023-12-10 09:28:16 -0600

    Send restored, re-arranged draft that includes the async package and comes with async/await in ALL areas; this version updates the model and not the instance; the former returns a promise while the latter returns a model instance

--- src/lambda/createonetransaction.js
+++ src/lambda/createonetransaction.js
@@ -36,98 +36,91 @@ exports.handler = async (event, context, callback) => {
 
   const decrementInventory = async (arrItems) => {
     
-    // await console.log( "decrementInventory arrItems", await arrItems );
+    await console.log( "decrementInventory arrItems", await arrItems );
 
-    // await async.forEachOf( 
-    return await forEachOf( 
-      
-      arrItems, 
-
-      async ( purchasedItem, idx, callback ) => { 
+    let cbFunk = async ( purchasedItem, idx, callback ) => { 
         
-        // await console.log( '\n\n' , "await purchasedItem", await purchasedItem );
+      // await console.log( '\n\n' , "await purchasedItem", await purchasedItem );
 
-        try {
-          
-          var findParam = {
-            where: {
-              id: { 
-                [Op.eq]: fn( 'UUID_TO_BIN' , await purchasedItem.id ) 
-              }
-            } 
-          };
-
-          // console.log( '\n\n' , "findParam", findParam );
-
-          let itemInStock = await Inventory.findOne( await findParam );
-          
-          // if (await itemInStock === null){
-          //   console.log( '\n\n' , 'Not found!');
-          // }
-          
-          if (await itemInStock !== null) {
-            
-            try{
-              
-              // await console.log( '\n\n' , 'await itemInStock', await itemInStock);
-              // await console.log('typeof await itemInStock.quantity', typeof await itemInStock.quantity); // number
-              // await console.log('typeof await purchasedItem.quantity', typeof await purchasedItem.quantity); // number
-
-              if( await itemInStock.quantity >= await purchasedItem.quantity ){
-                
-                // await console.log( '\n' , `Successfully purchased ${await purchasedItem.quantity} ${await purchasedItem.name}s!`);
-                
-                var optionsPm = await {
-                  where: {
-                    id: { 
-                      [Op.eq]: fn( 'UUID_TO_BIN' , await purchasedItem.id ) 
-                    }
-                  } 
-                };
-
-                // await console.log( 'await optionsPm', await optionsPm );
-
-                var qtyDiff = await itemInStock.quantity - await purchasedItem.quantity; 
-
-                const updatedQuantity = await {
-                  quantity: await qtyDiff
-                };
-
-                // let updatedinventory = await Inventory.update( await updatedQuantity , await optionsPm ); 
-
-                let updatedinventory = await itemInStock.update( await updatedQuantity , await optionsPm ); 
-                await console.log("await updatedinventory: " , await updatedinventory );
-                
-                // return await updatedinventory;
-                return updatedinventory;
-              }
-              else
-              if( await itemInStock.quantity < await purchasedItem.quantity ){
-                await console.error('\n' ,"Cannot complete this portion of transaction");
-                await console.error(`Attempted to buy ${await purchasedItem.quantity} ${await purchasedItem.name}s but there are only ${await itemInStock.quantity} available!`);
-              }
+      try {
+        
+        var findParam = {
+          where: {
+            id: { 
+              [Op.eq]: fn( 'UUID_TO_BIN' , await purchasedItem.id ) 
             }
-            catch(derrp){
-              console.log( '\n\n' , "try/catch derrp", derrp );
-              return callback(derrp);
-            }  
-          }
-        }
-        catch(e){
-          console.log( '\n\n' , "try/catch e", e );
-          return callback(e);
-        }
-      } , 
+          } 
+        };
 
-      err => {
-        if (err) {
-          console.error( '\n\n' , "err: " , err );
-          // console.error( '\n\n' , "err.message: " , err.message );
+        // console.log( '\n\n' , "findParam", findParam );
+
+        let itemInStock = await Inventory.findOne( await findParam );
+        
+        // if (await itemInStock === null){
+        //   console.log( '\n\n' , 'Not found!');
+        // }
+        
+        if (await itemInStock !== null) {
+          
+          try{
+            
+            // await console.log( '\n\n' , 'await itemInStock', await itemInStock);
+            // await console.log('typeof await itemInStock.quantity', typeof await itemInStock.quantity); // number
+            // await console.log('typeof await purchasedItem.quantity', typeof await purchasedItem.quantity); // number
+
+            if( await itemInStock.quantity >= await purchasedItem.quantity ){
+              
+              // await console.log( '\n' , `Successfully purchased ${await purchasedItem.quantity} ${await purchasedItem.name}s!`);
+              
+              var optionsPm = await {
+                where: {
+                  id: { 
+                    [Op.eq]: fn( 'UUID_TO_BIN' , await purchasedItem.id ) 
+                  }
+                } 
+              };
+
+              // await console.log( 'await optionsPm', await optionsPm );
+
+              var qtyDiff = await itemInStock.quantity - await purchasedItem.quantity; 
+
+              const updatedQuantity = await {
+                quantity: await qtyDiff
+              };
+
+              let updatedinventory = await Inventory.update( await updatedQuantity , await optionsPm ); // on a lark...
+              // let updatedinventory = await itemInStock.update( await updatedQuantity , await optionsPm ); // lkgc
+              await console.log("await updatedinventory: " , await updatedinventory );
+              
+              return await updatedinventory;
+              // return updatedinventory;
+            }
+            else
+            if( await itemInStock.quantity < await purchasedItem.quantity ){
+              await console.error('\n' ,"Cannot complete this portion of transaction");
+              await console.error(`Attempted to buy ${await purchasedItem.quantity} ${await purchasedItem.name}s but there are only ${await itemInStock.quantity} available!`);
+            }
+          }
+          catch(derrp){
+            console.log( '\n\n' , "try/catch derrp", derrp );
+            return callback(derrp);
+          }  
         }
       }
-
-    ); // END async.forEachOf()
+      catch(e){
+        console.log( '\n\n' , "try/catch e", e );
+        return callback(e);
+      }
+    };
     
+    let errorFunk = async (err) => {
+      if (err) {
+        console.error( '\n\n' , "err: " , err );
+        // console.error( '\n\n' , "err.message: " , err.message );
+      }
+    };
+    
+    return await forEachOf( await arrItems, await cbFunk , await errorFunk ); // END async.forEachOf()
 
   }; // END decrementInventory
   
